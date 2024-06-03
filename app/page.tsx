972a3a4c47a65e04264bc21a2f5a8d0356e2ae0f
はこだてたろう <hakodate.1106v@gmail.com>
2024-06-03 23:29:23 +0900

    親子関係のあるセレクトボックスAPI版

--- app/page.tsx
+++ app/page.tsx
@@ -57,6 +57,25 @@ export default function Home() {
           </p>
         </Link>
       </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <Link
+          href="/parent-child-select-api"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Parent-Child Select API{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            親子関係のあるセレクトボックスを出力します。
+          </p>
+        </Link>
+      </div>
     </main>
   );
 }
