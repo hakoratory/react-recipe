77c247349e0df36625c34c50258d046ca8e281a6
hakodio <hakodate.1106v@gmail.com>
2024-06-11 23:20:01 +0900

    ページリンクを追加

--- app/page.tsx
+++ app/page.tsx
@@ -95,6 +95,24 @@ export default function Home() {
               APIからデータを取得して、親子関係のあるセレクトボックスを出力します。フォームの入力値を state で管理します。
             </p>
           </Link>
+
+          <Link
+              href="/side-menu"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Side Menu{" "}
+              <span
+                  className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              最もシンプルな、固定長で静的なサイドメニューです。
+            </p>
+          </Link>
         </div>
       </main>
   );
