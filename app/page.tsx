92973521a0eee59ef0a9b3cbd9da9e95745574b7
hakodio <hakodate.1106v@gmail.com>
2024-06-16 00:05:10 +0900

    ページリンクの追加

--- app/page.tsx
+++ app/page.tsx
@@ -113,6 +113,42 @@ export default function Home() {
               最もシンプルな、固定長で静的なサイドメニューです。
             </p>
           </Link>
+
+          <Link
+              href="/side-menu-component"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Side Menu Component{" "}
+              <span
+                  className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              サイドメニューの項目をコンポーネント化しました。
+            </p>
+          </Link>
+
+          <Link
+              href="/side-menu-dynamic"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Side Menu Dynamic{" "}
+              <span
+                  className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              サイドメニューの項目を動的に出力します。
+            </p>
+          </Link>
         </div>
       </main>
   );
