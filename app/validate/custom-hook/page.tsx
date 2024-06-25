30a9dd66c137b0df0f494a871fd9437cb63a33cb
hakodio <hakodate.1106v@gmail.com>
2024-06-26 00:20:09 +0900

    バリデーション カスタムフック版：実装

--- app/validate/custom-hook/page.tsx
+++ app/validate/custom-hook/page.tsx
@@ -1,48 +1,67 @@
 'use client'
 
 import {Box, Button, Typography} from "@mui/material";
-import {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import {ChangeEvent, Dispatch, FormEvent, SetStateAction, useEffect, useState} from "react";
 import './style.css'
-import {ValidateError} from "@/types/validate-scratch";
-import {ValidateUseEffectForm} from "@/types/validate-use-effect";
 
-export default function ValidateCustomHook() {
-    const [formData, setFormData] = useState<ValidateUseEffectForm>({
-        fullName: '',
-        fullNameKana: '',
-    })
-    const [errors, setErrors] = useState<ValidateError[]>([])
+const useTextInput = (): [{ value: string, errors: string[]}, Dispatch<SetStateAction<string>>] => {
+    const [value, setValue] = useState<string>('')
+    const [errors, setErrors] = useState<string[]>([])
 
     useEffect(() => {
-        const validate = (propertyName: string, value: string): ValidateError[] => {
-            const errors: ValidateError[] = [];
+        const validate = (): string[] => {
+            const errors: string[] = [];
             if (value === '') {
-                errors.push({
-                    key: propertyName,
-                    message: '入力してください'
-                })
+                errors.push('入力してください')
             }
-            if ((value as string).length > 30) {
-                errors.push({
-                    key: propertyName,
-                    message: '30字以下で入力してください'
-                })
+            if (value.length > 30) {
+                errors.push('30字以下で入力してください')
             }
             return errors
         }
 
-        setErrors([
-            ...validate('fullName', formData.fullName),
-            ...validate('fullNameKana', formData.fullNameKana)
-        ])
+        setErrors(validate())
 
-    }, [formData]);
+    }, [value]);
+
+    return [{ value, errors }, setValue]
+}
+
+const useAlphaInput = (): [{ value: string, errors: string[]}, Dispatch<SetStateAction<string>>] => {
+    const [value, setValue] = useState<string>('')
+    const [errors, setErrors] = useState<string[]>([])
+
+    useEffect(() => {
+        const validate = (): string[] => {
+            const errors: string[] = [];
+            if (value === '') {
+                errors.push('入力してください')
+            }
+            if (value.length > 30) {
+                errors.push('30字以下で入力してください')
+            }
+            if (!value.match(/^[a-zA-Z ]*$/)) {
+                errors.push('半角英字で入力してください')
+            }
+            return errors
+        }
+
+        setErrors(validate())
+
+    }, [value]);
+
+    return [{ value, errors }, setValue]
+}
+
+export default function ValidateCustomHook() {
+    const [fullName, setFullName] = useTextInput()
+    const [fullNameKana, setFullNameKana] = useTextInput()
+    const [fullNameEnglish, setFullNameEnglish] = useAlphaInput()
 
     const onSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
 
-        setErrors(errors)
-        if (errors.length) {
+        if (fullName.errors.length || fullNameKana.errors.length) {
             return
         }
 
@@ -52,27 +71,19 @@ export default function ValidateCustomHook() {
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         switch (event.target.name) {
             case 'fullName': {
-                setFormData({...formData, fullName: event.target.value})
+                setFullName(event.target.value)
                 break
             }
             case 'fullNameKana': {
-                setFormData({...formData, fullNameKana: event.target.value})
+                setFullNameKana(event.target.value)
+                break
+            }
+            case 'fullNameEnglish': {
+                setFullNameEnglish(event.target.value)
                 break
             }
             default:
         }
-    }
-
-    const getErrorByPropertyName = (propertyName: string) => {
-        return errors.filter(error => error.key === propertyName)
-    }
-
-    const getErrorMessageByPropertyName = (propertyName: string) => {
-        return getErrorByPropertyName(propertyName).map((error, index) => <span key={`${index}-${error.key}`}>{error.message}</span>)
-    }
-
-    const isError = (propertyName: string) => {
-        return getErrorByPropertyName(propertyName).length > 0
     }
 
     return (
@@ -84,11 +95,11 @@ export default function ValidateCustomHook() {
                             <span>名前</span>
                         </Box>
                         <Box>
-                            <input type="text" name="fullName" className={isError('fullName') ? 'error' : ''} onChange={handleChange} />
+                            <input type="text" name="fullName" className={fullName.errors.length ? 'error' : ''} onChange={handleChange} />
                         </Box>
                         <Box height="1rem" sx={{color: 'red'}}>
                             {
-                                getErrorMessageByPropertyName('fullName')
+                                fullName.errors.map((error, index) => <span key={`${index}-fullName`}>{error}</span>)
                             }
                         </Box>
                     </Box>
@@ -97,28 +108,41 @@ export default function ValidateCustomHook() {
                             <span>かな</span>
                         </Box>
                         <Box>
-                            <input type="text" name="fullNameKana" className={isError('fullNameKana') ? 'error' : ''} onChange={handleChange} />
+                            <input type="text" name="fullNameKana" className={fullNameKana.errors.length ? 'error' : ''} onChange={handleChange} />
                         </Box>
                         <Box height="1rem" sx={{color: 'red'}}>
                             {
-                                getErrorMessageByPropertyName('fullNameKana')
+                                fullNameKana.errors.map((error, index) => <span key={`${index}-fullNameKana`}>{error}</span>)
+                            }
+                        </Box>
+                    </Box>
+                    <Box mb={2}>
+                        <Box mb={1}>
+                            <span>Full Name</span>
+                        </Box>
+                        <Box>
+                            <input type="text" name="fullNameEnglish" className={fullNameEnglish.errors.length ? 'error' : ''} onChange={handleChange} />
+                        </Box>
+                        <Box height="1rem" sx={{color: 'red'}}>
+                            {
+                                fullNameEnglish.errors.map((error, index) => <span key={`${index}-fullNameEnglish`}>{error}</span>)
                             }
                         </Box>
                     </Box>
                     <Box>
-                        <Button type="submit" variant="contained" disabled={errors.length > 0}>SUBMIT</Button>
+                        <Button type="submit" variant="contained" disabled={fullName.errors.length > 0 || fullNameKana.errors.length > 0}>SUBMIT</Button>
                     </Box>
                 </form>
             </Box>
             <Box mt={2} ml={2}>
                 <Box>
                     <Typography variant="body2">
-                        {formData.fullNameKana !== '' && `${formData.fullNameKana}さん、こんにちは。` }
+                        {fullNameKana.value !== '' && `${fullNameKana.value}さん、こんにちは。` }
                     </Typography>
                 </Box>
                 <Box>
                     <Typography variant="h4">
-                        {formData.fullName !== '' && `${formData.fullName}さん、こんにちは。` }
+                        {fullName.value !== '' && `${fullName.value}さん、こんにちは。` }
                     </Typography>
                 </Box>
             </Box>
